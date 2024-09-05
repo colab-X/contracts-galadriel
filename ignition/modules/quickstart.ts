a24b4a13fe4f74ffe997bdd9b092ae66db286322
Nasfame <laciferin2024@protonmail.com>
2024-09-06 04:52:03 +0530

    from:

--- ignition/modules/quickstart.ts
+++ ignition/modules/quickstart.ts
@@ -1,12 +1,20 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
 
+import hre from "hardhat"
+
 const ORACLE_ADDRESS = process.env.ORACLE_ADDRESS
 if (!ORACLE_ADDRESS) {
   throw new Error("ORACLE_ADDRESS env variable is not set.")
 }
 
 export default buildModule("Quickstart", (m) => {
-  const quickStart = m.contract("Quickstart", [ORACLE_ADDRESS])
+  const admin = m.getAccount(0)
+
+  console.log({ admin })
+
+  const quickStart = m.contract("Quickstart", [ORACLE_ADDRESS], {
+    // from: admin,
+  })
 
   return { quickStart }
 })
