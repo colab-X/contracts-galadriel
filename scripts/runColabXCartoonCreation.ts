8b859d224e61003a8b6ddb3bf58c80cba07baed0
Nasfame <laciferin2024@protonmail.com>
2024-09-08 01:15:57 +0530

    crazy
    Took 26 minutes

--- scripts/runColabXCartoonCreation.ts
+++ scripts/runColabXCartoonCreation.ts
@@ -1,90 +1,101 @@
 // Import ethers from Hardhat package
 import readline from "readline"
 
-import { deployments } from "hardhat"
-import { connectContract, getWallet } from "../utils/web3"
+import {deployments} from "hardhat"
+import {connectContract, getWallet} from "../utils/web3"
 import {ColabXCartoonCreation, Quickstart} from "typechain-types"
-import { getAccount } from "utils/accounts"
+import {getAccount} from "utils/accounts"
 import bluebird from "bluebird"
 import JobStruct = ColabXCartoonCreation.JobStruct;
+import {JobAddedEvent} from "../typechain-types/contracts/ColabXOnChainJobCreator";
 
 async function main() {
-  const contractName = "ColabXCartoonCreation"
-  // const quickStartContract = await deployments.get(contractName)
+    const contractName = "ColabXCartoonCreation"
+    // const quickStartContract = await deployments.get(contractName)
 
-  const quickStart = await connectContract<ColabXCartoonCreation>(contractName)
-  const contract = quickStart.connect(getWallet("admin"))
+    const quickStart = await connectContract<ColabXCartoonCreation>(contractName)
+    const contract = quickStart.connect(getWallet("admin"))
 
-  // Create a contract instance
-  // const contract = new ethers.Contract(contractAddress, contractABI, signer)
+    // Create a contract instance
+    // const contract = new ethers.Contract(contractAddress, contractABI, signer)
 
-  // The content of the image you want to generate
-  const message = await getUserInput()
+    // The content of the image you want to generate
+    // const message: string = await getUserInput()
+    const message: string = "tom and jerry"
+    console.log(`Image generation started with message: "${message}"`)
 
-  // Call the startChat function
-  const transactionResponse = await contract.initJob(message)
-  const receipt = await transactionResponse.wait()
-  console.log(
-    `Transaction sent, hash: ${receipt.hash}.\nExplorer: https://explorer.galadriel.com/tx/${receipt.hash}`
-  )
-  console.log(`Image generation started with message: "${message}"`)
+    // Call the startChat function
+    const transactionResponse = await contract.initJob(message)
+    const receipt = await transactionResponse.wait()
+    console.log(
+        `Transaction sent, hash: ${receipt.hash}.\nExplorer: https://explorer.galadriel.com/tx/${receipt.hash}`
+    )
 
-  // console.log({receipt})
+    // console.log({receipt})
 
-  // const jobId = await transactionResponse
+    // const jobId = await transactionResponse
 
-  let jobId = 0;
+    let jobId ;
 
-  receipt.logs.forEach((log) => {
-    const logs = contract.interface.parseLog(log as any);
-    if (!logs) return;
-    jobId = Number(logs.args[0]);
-  });
+    receipt.logs.forEach((log) => {
+        const logs = contract.interface.parseLog(log as any);
+        if (!logs) return;
+        jobId = Number(logs.args[0]);
+    });
 
-  console.log(`Job ID: ${jobId}`)
+    // jobId = transactionResponse.toString()
 
-  let job = await contract.getJobDetails(jobId)
+    console.log(`Job ID: ${jobId}`)
 
-  // print w/o newline
-  console.log("Waiting for response: ")
+    let job = await contract.getJobDetails(jobId)
 
-console.log({job})
-  while (job.status!=0) {
-    await bluebird.delay(1000)
-    job = await contract.getJobStatus()
-    console.log(".")
-  }
+    // print w/o newline
+    console.log("Waiting for response: ")
 
-  console.log(`Image generation completed, image URL: ${job.response}`)
+
+    // jobStatus started
+    while (job.status.toString() == '0')  {
+        console.log({
+            id: job.id.toString(),
+            jobStatus: job.status.toString(),
+            message: job.message,
+            response: job.response,
+        })
+        await bluebird.delay(1000)
+        job = await contract.getJobDetails(jobId)
+        console.log(".")
+    }
+
+    console.log(`Image generation completed, image URL: ${job.response}`)
 }
 
 async function getUserInput(): Promise<string | undefined> {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  })
-
-  const question = (query: string): Promise<string> => {
-    return new Promise((resolve) => {
-      rl.question(query, (answer) => {
-        resolve(answer)
-      })
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
     })
-  }
 
-  try {
-    const input = await question("Enter an image description: ")
-    rl.close()
-    return input
-  } catch (err) {
-    console.error("Error getting user input:", err)
-    rl.close()
-  }
+    const question = (query: string): Promise<string> => {
+        return new Promise((resolve) => {
+            rl.question(query, (answer) => {
+                resolve(answer)
+            })
+        })
+    }
+
+    try {
+        const input = await question("Enter an image description: ")
+        rl.close()
+        return input
+    } catch (err) {
+        console.error("Error getting user input:", err)
+        rl.close()
+    }
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
